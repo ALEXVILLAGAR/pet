743c47e65ccc71efc24334b77200344ccffdd275
DESKTOP-T4UG1Q4\milev <alexvilla@utp.edu.co>
2018-08-09 11:12:34 -0500

    identando

--- ruta.php
+++ ruta.php
@@ -1,34 +1,30 @@
 <?php
-require_once('ControlClass.php');
+require_once 'ControlClass.php';
 
 $variable = $_GET['variable'];
 switch ($variable) {
-	case 'login':
-			$ctrl = new Control();
-			$ctrl->login();
-		break;
-	case 'cerrar_login':
-		Control::cerrar_sesion();
-		break;
-	case 'agregar_mascota':
-		//Control::cerrar_sesion();
-	break;
-	case 'login_fundacion':
-			$ctrl = new Control();
-			$ctrl->login('fundacion');
-		break;
-	// case 'users':
-	// 	$user = new Usuario();
-	// 	$user->todos_usuarios();
-	// break;
-	// case 'login':
-	// 	# code...
-	// 	break;
-	default:
-		# code...
-		break;
+    case 'login':
+        $ctrl = new Control();
+        $ctrl->login();
+        break;
+    case 'cerrar_login':
+        Control::cerrar_sesion();
+        break;
+    case 'agregar_mascota':
+        //Control::cerrar_sesion();
+        break;
+    case 'login_fundacion':
+        $ctrl = new Control();
+        $ctrl->login('fundacion');
+        break;
+    // case 'users':
+    //     $user = new Usuario();
+    //     $user->todos_usuarios();
+    // break;
+    // case 'login':
+    //     # code...
+    //     break;
+    default:
+        # code...
+        break;
 }
-
-
-
-?>
