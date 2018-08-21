ec1e0e671e9d62651b0f2640b7fb99676dccdc7f
Camilo <camilo07@utp.edu.co>
2018-08-20 23:53:21 -0500

    vista usuario administrador

--- views/perfil/perfiladmin.php
+++ views/perfil/perfiladmin.php
@@ -182,28 +182,28 @@
         </nav>
         <!-- sidebar-wrapper  -->
         <section class="bg-faded page-content">
-            <?php include 'tablamascotasadmin.php'?>
-            <?php include 'donar.html'?>
+            <?php include '..\tablas\tablamascotasadmin.php'?>
+
         </section>
         <!-- page-content" -->
     </div>
     <!-- page-wrapper -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js">
+    <script src="../../vendor/jquery-easing/jquery.easing.min.js">
     </script>
-    <script src="vendor/scrollreveal/scrollreveal.min.js">
+    <script src="../../vendor/scrollreveal/scrollreveal.min.js">
     </script>
-    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js">
+    <script src="../../vendor/magnific-popup/jquery.magnific-popup.min.js">
     </script>
-    <script src="vendor/jquery/jquery.min.js">
+    <script src="../../vendor/jquery/jquery.min.js">
     </script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js">
+    <script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js">
     </script>
-    <script src="js/custom.js">
+    <script src="../../js/custom.js">
     </script>
-    <script src="js/pinterest_grid.js">
+    <script src="../../js/pinterest_grid.js">
     </script>
-    <script src="js/creative.min.js">
+    <script src="../../js/creative.min.js">
     </script>
-    <script src="js/main.js">
+    <script src="../../js/main.js">
     </script>
 </body>
